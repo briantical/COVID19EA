ed324bc792e3449871fecbbaf12f222ed418802e
briantical <lutbrianivan@gmail.com>
2020-04-02 16:40:01 +0300

    configuring autosuggest

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,119 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Autosuggest from "react-autosuggest";
+
+const logo = require("./../assets/header_logo.png");
+const WHO = require("./../assets/who.png");
+
+// Imagine you have a list of countries that you'd like to autosuggest.
+const languages = [
+  {
+    name: "Uganda"
+  },
+  {
+    name: "Kenya"
+  },
+  {
+    name: "Tanzania"
+  },
+  {
+    name: "Rwanda"
+  },
+  {
+    name: "Burundi"
+  }
+];
+
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0
+    ? []
+    : languages.filter(
+        lang => lang.name.toLowerCase().slice(0, inputLength) === inputValue
+      );
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => <div>{suggestion.name}</div>;
 
 export class Home extends Component {
+  constructor() {
+    super();
+
+    // Autosuggest is a controlled component.
+    // This means that you need to provide an input value
+    // and an onChange handler that updates this value (see below).
+    // Suggestions also need to be provided to the Autosuggest,
+    // and they are initially empty because the Autosuggest is closed.
+    this.state = {
+      value: "",
+      suggestions: []
+    };
+  }
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
   render() {
-    return <div>Home</div>;
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: "Type a programming language",
+      value,
+      onChange: this.onChange
+    };
+
+    // Finally, render it!
+    return (
+      <div>
+        <header>
+          <a href="/resources">Resources</a>
+          <a href="/support">Support</a>
+          <img
+            src={logo}
+            alt="COVID19EA Logo"
+            title="COVID19EA Logo"
+            id="header_logo"
+          />
+        </header>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+        />
+        <div id="who_logo">
+          <img src={WHO} alt="WHO Log" title="Data provided by WHO" />
+        </div>
+      </div>
+    );
   }
 }
 
