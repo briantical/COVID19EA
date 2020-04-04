3952418debd330fa956da02b8868544b775b4772
briantical <lutbrianivan@gmail.com>
2020-04-05 02:02:51 +0300

    Initializing Heroku

--- src/containers/Country.js
+++ src/containers/Country.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { Header } from "./components";
+const WHO = require("./../assets/who.png");
+
 export class Country extends Component {
   render() {
-    return <div>Country Data</div>;
+    return (
+      <div className="container-fluid no-gutters px-0">
+        <Header />
+        <div className="content commons_vertical">Country Data</div>
+        <img
+          src={WHO}
+          alt="WHO Log"
+          title="Data provided by WHO"
+          className="img-fluid"
+          id="who_logo"
+        />
+      </div>
+    );
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = (state) => ({});
 
 const mapDispatchToProps = {};
 
