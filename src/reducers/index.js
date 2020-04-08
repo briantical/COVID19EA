a5435aba8f4ecc4a79f0f0faf35a8fc5cf662341
briantical <lutbrianivan@gmail.com>
2020-04-08 13:18:22 +0300

    Resolving conflicts

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import error from "./error";
-import report from "./report";
-import reports from "./reports";
-import typing from "./typing";
-import suggestions from "./suggestions";
-
-export default combineReducers({ error, typing, report, reports, suggestions });
