4802b58105b3062656a91629be097d0a82b48b2d
briantical <lutbrianivan@gmail.com>
2020-05-26 11:35:21 +0300

    New structure

--- client/src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Country } from "./Country";
-export { default as Region } from "./Region";
-export { default as NoMatch } from "./NoMatch";
-export { default as Home } from "./Home";
