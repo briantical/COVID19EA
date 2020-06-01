da1d77bdc03f84d10b1824d1f07c0276110a9624
briantical <lutbrianivan@gmail.com>
2020-06-01 12:52:58 +0300

    Configuring Heroku

--- client/src/containers/NoMatch/index.tsx
+++ client/src/containers/NoMatch/index.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const NoMatch: FC<{}> = () => {
-  return <div>No Matching Route</div>;
+  return (
+    <div>
+      No Matching Route<Link to="/">Home</Link>
+    </div>
+  );
 };
 
 export default NoMatch;
